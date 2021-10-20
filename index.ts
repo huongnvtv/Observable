4dafef5d3b4ea59d7256239e69af87afdfba18bf
huongnvtv <huongitnv@gmail.com>
2021-10-20 14:44:24 +0700

    commit rxjs filter operator

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 // Import stylesheets
 import './style.css';
-import { from, fromEvent, Observable, of } from 'rxjs';
-import { filter } from 'rxjs/operators';
+import { from, fromEvent, Observable, of ,interval, timer } from 'rxjs';
+import { filter, find, single, take, takeUntil } from 'rxjs/operators';
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
@@ -17,8 +17,9 @@ const observer = {
     console.log('Done');
   },
 };
-from([1,2,3,4,5,6]).pipe(
-   filter(item => {
-     return item %2 ===0;
-   })
-).subscribe(console.log);
+// from([1,2,3,4,5,6]).pipe(
+//    take(2)
+// ).subscribe(console.log);
+interval(1000).pipe(
+   takeUntil(fromEvent(document,'click'))
+).subscribe(observer);
