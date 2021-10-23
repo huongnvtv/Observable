225c15a16f24af93c9f7855f209dfa1994fd39ef
huongnvtv <huongitnv@gmail.com>
2021-10-23 09:00:16 +0700

    commit forkJoin

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 // Import stylesheets
 import './style.css';
-import { from, fromEvent, Observable, of ,interval, timer } from 'rxjs';
-import { distinct, filter, find, single, skip, take, takeUntil, takeWhile } from 'rxjs/operators';
+import { from, fromEvent, Observable, of ,interval, timer, forkJoin, delay } from 'rxjs';
+import { distinct, filter, find, map, single, skip, take, takeUntil, takeWhile } from 'rxjs/operators';
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
@@ -17,6 +17,11 @@ const observer = {
     console.log('Done');
   },
 };
-from([1,2,3,4,5,4,3,2,1]).pipe(
-   distinct()
-).subscribe(observer);
+forkJoin([
+  of([1,2,3]).pipe(delay(2000)),
+  of("word").pipe(delay(5000))
+]).pipe(
+  map(function([item1,item2]){
+    return {hello: item1, word : item2}
+  })
+).subscribe(observer)
